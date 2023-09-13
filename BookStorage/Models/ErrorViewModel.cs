da79cfa2e5658ea59d994b74a4a32d9251f65845
Alex Corman <alex@nyble.co.uk>
2023-09-13 19:36:55 +0300

    Add project files.

---
+++ BookStorage/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BookStorage.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
