0d8d91f4baf0e135eca0b6f2d5aedfc99b6e4df7
Alex Corman <alex@nyble.co.uk>
2023-09-15 00:15:27 +0300

    Added vue.js & cleanup

---
+++ BookStorage/wwwroot/js/home.js
@@ -0,0 +1,13 @@
+﻿let homeApp;
+
+homeApp = new Vue({
+    el: '#home-app',
+    data: {
+        message: 'Hello Vue!'
+    },
+    methods: {
+        setMessage: function (event) {
+            this.message = event.target.value;
+        }
+    }
+});
